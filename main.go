1124a597ac8ae7ebca50c7a786999f186f87d126
Nicolas Chalanset <nicolas@chalanset.fr>
2019-10-03 09:11:06 +0200

    restructuring

--- main.go
+++ main.go
@@ -2,19 +2,14 @@ package main // import "github.com/nicocha/rebopagent"
 
 import (
 	"bytes"
-	"crypto/x509"
-	"encoding/base64"
-	"encoding/pem"
 	"errors"
 
 	//	"flag"
 
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
 	"os"
-	"path/filepath"
 	"strings"
 	"sync"
 	"time"
@@ -190,117 +185,4 @@ func stringInSlice(str string, list []string) bool {
 		}
 	}
 	return false
-}
-
-func parseHostForCertFiles(pathS string, paths chan fsEntry, errs chan error, wg *sync.WaitGroup) {
-	defer wg.Done()
-	//certpool, _ := x509.SystemCertPool()
-	//fmt.Println(certpool.Subjects())
-
-	//var pathS string = "/Users/nicocha/Projects/"
-	//var data string
-	var i int = 0
-
-	filepath.Walk(pathS, func(path string, f os.FileInfo, err error) error {
-		i++
-		if err != nil {
-			/*
-				for i := 0; i < 100; i++ {
-					log.Println("**********************************************")
-				}
-				log.Println(err)
-				log.Println("**********************************************")
-			*/
-			errs <- err
-			return nil
-		}
-		if !f.IsDir() {
-			paths <- fsEntry{path: path, f: f}
-		}
-		/*
-			if i%100 == 0 {
-				fmt.Println(i, len(paths), time.Since(start), parsedCount, validCount)
-			}
-		*/
-		return nil
-	})
-}
-
-func worker(entries chan fsEntry, errs chan error, certs chan *certificate, wg *sync.WaitGroup) {
-	for entry := range entries {
-		wg.Add(1)
-		// too fast, need to save entry value before executing fo routine
-		entryCopy := entry
-		go func() {
-			defer wg.Done()
-			// FIXME: use a limited pool
-			cert, err := once(entryCopy)
-			if err != nil {
-				errs <- err
-				return
-			}
-			if cert != nil {
-				certs <- cert
-			}
-		}()
-	}
-}
-
-func once(entry fsEntry) (*certificate, error) {
-	var cert string
-
-	// todo: give the task to a worker pool
-	//fmt.Println(filepath.Ext(entry.path))
-	if stringInSlice(filepath.Ext(entry.path), ext) {
-		mutex.Lock()
-		parsedCount++
-		mutex.Unlock()
-
-		dat, err := ioutil.ReadFile(entry.path)
-		if err != nil {
-			return nil, err
-		}
-		//fmt.Println(filepath.Ext(entry.path))
-		if cap(dat) > 0 {
-			//fmt.Println("CAP")
-			if !strings.Contains(string(dat), ("PRIVATE KEY")) && !strings.Contains(string(dat), ("PUBLIC KEY")) && !strings.Contains(string(dat), ("-----BEGIN CERTIFICATE-----")) {
-				cert = base64.StdEncoding.EncodeToString(dat)
-				cert = insertNth(cert, 64)
-				cert = "-----BEGIN CERTIFICATE-----" + "\n" + cert + "\n" + "-----END CERTIFICATE-----"
-			} else {
-				//fmt.Println("ELSE")
-				cert = string(dat)
-				block, _ := pem.Decode([]byte(cert))
-				if block == nil {
-					fmt.Println("failed to parse PEM file: ", entry.path)
-				} else {
-					//fmt.Println(block.Bytes)
-					_, err := x509.ParseCertificate(block.Bytes)
-					if err != nil {
-						if strings.Contains(err.Error(), "named curve") {
-							fmt.Println(err.Error())
-						} else {
-							return nil, fmt.Errorf(err.Error(), entry.path)
-						}
-					}
-					//fmt.Println("AFTER RETURN")
-					certificate := certificate{
-						hostname,
-						"",
-						ipaddress,
-						entry.f.Name(),
-						entry.path,
-						cert,
-						time.Now().UTC().Format("2006-01-02T15:04:05z"),
-						"local",
-					}
-					mutex.Lock()
-					validCount++
-					mutex.Unlock()
-					return &certificate, nil
-				}
-			}
-		}
-	}
-	return nil, nil
 }
--- rebopScan.go
+++ rebopScan.go
@@ -1,27 +1,24 @@
 package main
 
 import (
+	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 )
 
 func rebopScan(rootPath string) ([]byte, error) {
 	if _, err := os.Stat(rootPath); os.IsNotExist(err) {
-		log.Println("Couldn't open %s", rootPath)
-		return nil, nil
+		fmt.Printf("Couldn't open %s", rootPath)
+		return nil, err
 	}
-	//else {
-	/*
-		var rootPath, err = filepath.Abs(os.Args[1])
-		if err != nil {
-			fmt.Errorf(err.Error())
-			return nil
-		}
-	*/
 	var wg sync.WaitGroup
 
 	paths := make(chan fsEntry, 1)
@@ -31,7 +28,7 @@ func rebopScan(rootPath string) ([]byte, error) {
 
 	wg.Add(1)
 	go parseHostForCertFiles(rootPath, paths, errs, &wg)
-	go worker(paths, errs, certs, &wg)
+	go certWorker(paths, errs, certs, &wg)
 	go func() {
 		wg.Wait()
 		done <- true
@@ -43,13 +40,12 @@ func rebopScan(rootPath string) ([]byte, error) {
 		select {
 		case <-done:
 			mutex.Lock()
-			fmt.Println("Done in:", time.Since(start), "\nParsed", parsedCount, "files\nSaved", len(certificates), "out of", validCount, "certificates")
+			fmt.Println("Done in:", time.Since(start), "\nParsed", parsedCount, "files\nFound", len(certificates), "out of", validCount, "certificates")
 			mutex.Unlock()
 
 			certificateJSON, err := json.Marshal(certificates)
 			if err != nil {
-				fmt.Errorf(err.Error())
-				os.Exit(1)
+				return nil, err
 			}
 			//fmt.Printf("Name: %s\nComment: %s\nModTime: %s\n\n", zw.Name, zw.Comment, zw.ModTime.UTC())
 			return certificateJSON, nil
@@ -59,5 +55,117 @@ func rebopScan(rootPath string) ([]byte, error) {
 			fmt.Println("error: ", err)
 		}
 	}
-	//}
+}
+
+func parseHostForCertFiles(pathS string, paths chan fsEntry, errs chan error, wg *sync.WaitGroup) {
+	defer wg.Done()
+	//certpool, _ := x509.SystemCertPool()
+	//fmt.Println(certpool.Subjects())
+
+	//var pathS string = "/Users/nicocha/Projects/"
+	//var data string
+	var i int = 0
+
+	filepath.Walk(pathS, func(path string, f os.FileInfo, err error) error {
+		i++
+		if err != nil {
+			/*
+				for i := 0; i < 100; i++ {
+					log.Println("**********************************************")
+				}
+				log.Println(err)
+				log.Println("**********************************************")
+			*/
+			errs <- err
+			return nil
+		}
+		if !f.IsDir() {
+			paths <- fsEntry{path: path, f: f}
+		}
+		/*
+			if i%100 == 0 {
+				fmt.Println(i, len(paths), time.Since(start), parsedCount, validCount)
+			}
+		*/
+		return nil
+	})
+}
+
+func certWorker(entries chan fsEntry, errs chan error, certs chan *certificate, wg *sync.WaitGroup) {
+	for entry := range entries {
+		wg.Add(1)
+		// too fast, need to save entry value before executing fo routine
+		entryCopy := entry
+		go func() {
+			defer wg.Done()
+			// FIXME: use a limited pool
+			cert, err := parseEntry(entryCopy)
+			if err != nil {
+				errs <- err
+				return
+			}
+			if cert != nil {
+				certs <- cert
+			}
+		}()
+	}
+}
+
+func parseEntry(entry fsEntry) (*certificate, error) {
+	var cert string
+
+	// todo: give the task to a worker pool
+	//fmt.Println(filepath.Ext(entry.path))
+	if stringInSlice(filepath.Ext(entry.path), ext) {
+		mutex.Lock()
+		parsedCount++
+		mutex.Unlock()
+
+		dat, err := ioutil.ReadFile(entry.path)
+		if err != nil {
+			return nil, err
+		}
+		//fmt.Println(filepath.Ext(entry.path))
+		if cap(dat) > 0 {
+			//fmt.Println("CAP")
+			if !strings.Contains(string(dat), ("PRIVATE KEY")) && !strings.Contains(string(dat), ("PUBLIC KEY")) && !strings.Contains(string(dat), ("-----BEGIN CERTIFICATE-----")) {
+				cert = base64.StdEncoding.EncodeToString(dat)
+				cert = insertNth(cert, 64)
+				cert = "-----BEGIN CERTIFICATE-----" + "\n" + cert + "\n" + "-----END CERTIFICATE-----"
+			} else {
+				//fmt.Println("ELSE")
+				cert = string(dat)
+				block, _ := pem.Decode([]byte(cert))
+				if block == nil {
+					fmt.Println("failed to parse PEM file: ", entry.path)
+				} else {
+					//fmt.Println(block.Bytes)
+					_, err := x509.ParseCertificate(block.Bytes)
+					if err != nil {
+						if strings.Contains(err.Error(), "named curve") {
+							fmt.Println(err.Error())
+						} else {
+							return nil, fmt.Errorf(err.Error(), entry.path)
+						}
+					}
+					//fmt.Println("AFTER RETURN")
+					certificate := certificate{
+						hostname,
+						"",
+						ipaddress,
+						entry.f.Name(),
+						entry.path,
+						cert,
+						time.Now().UTC().Format("2006-01-02T15:04:05z"),
+						"local",
+					}
+					mutex.Lock()
+					validCount++
+					mutex.Unlock()
+					return &certificate, nil
+				}
+			}
+		}
+	}
+	return nil, nil
 }
