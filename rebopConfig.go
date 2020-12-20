73748b3dceb04606d73335140bf90101a164e32d
Nicolas Chalanset <nicolas@chalanset.fr>
2020-12-20 19:08:07 +0100

    update agent name

--- rebopConfig.go
+++ rebopConfig.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"os"
@@ -13,16 +14,18 @@ import (
 
 // getConfigFileName ... "Get rebop config file name from ENV"
 func getConfigFileName() string {
-	env := os.Getenv("ENV")
-	if len(env) == 0 {
-		env = "production"
+	env := os.Getenv("REBOP_ENV")
+	if len(env) != 0 {
+		env = "." + env
+	} else {
+		env = ""
 	}
 
 	dirname, err := os.Executable()
 	if err != nil {
 		log.Println(err)
 	}
-	filename := []string{"/config/", "config.", env, ".yml"}
+	filename := []string{"/config/", "config", env, ".yml"}
 	//_, dirname, _, _ := runtime.Caller(0)
 	filePath := path.Join(filepath.Dir(dirname), strings.Join(filename, ""))
 
@@ -45,4 +48,40 @@ func getrebopConfig(cfg *Config) {
 	if err != nil {
 		processError(err)
 	}
+}
+
+// Local DB
+
+// Load loads the file at path into v.
+// Use os.IsNotExist() to see if the returned error is due
+// to the file being missing.
+func loadLocalDB(path string, v interface{}) error {
+	// Open a RO file
+	decodeFile, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer decodeFile.Close()
+	// Create a decoder
+	decoder := gob.NewDecoder(decodeFile)
+
+	decoder.Decode(v)
+	return err
+}
+
+// Save saves a representation of v to the file at path.
+func saveLocaDB(path string, v interface{}) error {
+	// Create a file for IO
+	encodeFile, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	// Since this is a binary format large parts of it will be unreadable
+	encoder := gob.NewEncoder(encodeFile)
+	// Write to the file
+	if err := encoder.Encode(v); err != nil {
+		return err
+	}
+	encodeFile.Close()
+	return err
 }
--- script-build-probes.sh
+++ script-build-probes.sh
@@ -1,48 +1,48 @@
 # https://www.digitalocean.com/community/tutorials/how-to-build-go-executables-for-multiple-platforms-on-ubuntu-16-04
 
 # Create dir
-mkdir -p rebopagents/windows/32bits/config/ rebopagents/windows/64bits/config/ rebopagents/linux/32bits/config rebopagents/linux/64bits/config rebopagents/macosx/config/
+mkdir -p reBop-agents/windows/32bits/config/ reBop-agents/windows/64bits/config/ reBop-agents/linux/32bits/config reBop-agents/linux/64bits/config reBop-agents/macosx/config/
 
 # Copy default prod config files
 
-cp config/config.production.yml rebopagents/windows/32bits/config/config.production.yml
-cp config/config.production.yml rebopagents/windows/64bits/config/config.production.yml
-cp config/config.production.yml rebopagents/linux/32bits/config/config.production.yml
-cp config/config.production.yml rebopagents/linux/64bits/config/config.production.yml
-cp config/config.production.yml rebopagents/macosx/config/config.production.yml
+cp config/config.yml reBop-agents/windows/32bits/config/config.yml
+cp config/config.yml reBop-agents/windows/64bits/config/config.yml
+cp config/config.yml reBop-agents/linux/32bits/config/config.yml
+cp config/config.yml reBop-agents/linux/64bits/config/config.yml
+cp config/config.yml reBop-agents/macosx/config/config.yml
 
 # Generate executables
 
-echo "Building rebopagents ..."
+echo "Building reBop-agents ..."
 
 # Windows
-GOOS=windows GOARCH=386 go build -o ./rebopagents/windows/32bits/rebopagent.exe
-GOOS=windows GOARCH=amd64 go build -o ./rebopagents/windows/64bits/rebopagent.exe
+GOOS=windows GOARCH=386 go build -o ./reBop-agents/windows/32bits/reBop-agent.exe
+GOOS=windows GOARCH=amd64 go build -o ./reBop-agents/windows/64bits/reBop-agent.exe
 
 # Linux
-GOOS=linux GOARCH=386 go build -o ./rebopagents/linux/32bits/rebopagent
-GOOS=linux GOARCH=amd64 go build -o ./rebopagents/linux/64bits/rebopagent
+GOOS=linux GOARCH=386 go build -o ./reBop-agents/linux/32bits/reBop-agent
+GOOS=linux GOARCH=amd64 go build -o ./reBop-agents/linux/64bits/reBop-agent
 
 # MacOS
-GOOS=darwin GOARCH=amd64 go build -o ./rebopagents/macosx/rebopagent
+GOOS=darwin GOARCH=amd64 go build -o ./reBop-agents/macosx/reBop-agent
 
 # Generate Zip files
 
 echo "Generating zip files ..."
 
-cd rebopagents/windows/32bits/
-zip ../../rebopagent-32bits-windows.zip rebopagent.exe config/config.production.yml
+cd reBop-agents/windows/32bits/
+zip ../../reBop-agent-32bits-windows.zip reBop-agent.exe config/config.yml
 
 cd ../64bits/
-zip ../../rebopagent-64bits-windows.zip rebopagent.exe config/config.production.yml
+zip ../../reBop-agent-64bits-windows.zip reBop-agent.exe config/config.yml
 
 cd ../../linux/32bits/
-zip ../../rebopagent-32bits-linux.zip rebopagent config/config.production.yml
+zip ../../reBop-agent-32bits-linux.zip reBop-agent config/config.yml
 
 cd ../64bits/
-zip ../../rebopagent-64bits-linux.zip rebopagent config/config.production.yml
+zip ../../reBop-agent-64bits-linux.zip reBop-agent config/config.yml
 
 cd ../../macosx/
-zip ../rebopagent-macosx.zip rebopagent config/config.production.yml
+zip ../reBop-agent-macosx.zip reBop-agent config/config.yml
 
 echo "Done"
